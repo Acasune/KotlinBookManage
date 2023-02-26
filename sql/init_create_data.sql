b79ce5a04dc3a7344ba3efbe83569fe10e7acb53
acasune <tttt4aaaa@gmail.com>
2023-02-26 14:35:05 +0900

    add db

---
+++ sql/init_create_data.sql
@@ -0,0 +1,3 @@
+﻿INSERT INTO book VALUES (1,'Kotlin入門','ことりん太郎','1950-10-01'), (200,'Java入門','ジャヴァ太郎','2005-08-29');
+-- password -> p@ssw0rd
+INSERT INTO user VALUES (1,'admin@test.com','$2y$05$BthLp.VIhe7BQwUll2nwme9RbvqY3p2pPacgUAfmfp.3Vv/5.w0ku','管理者','admin'), (2,'user@test.com','$2y$05$BthLp.VIhe7BQwUll2nwme9RbvqY3p2pPacgUAfmfp.3Vv/5.w0ku','ユーザ','USER');
